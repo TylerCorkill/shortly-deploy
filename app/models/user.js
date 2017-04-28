4a4145a8f50a38ad80d5a8715f37d030982281bd
TylerCorkill <TylerCorkill@gmail.com>
2017-04-28 08:32:27 -0700

    added concurrent to grunt

--- app/models/user.js
+++ app/models/user.js
@@ -3,17 +3,17 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var userSchema = mongoose.Schema({
+var userSchema = new mongoose.Schema({
   username: {type: String, required: true, index: {unique: true}},
   password: {type: String, required: true}
 });
 
-var User = mongoose.model('User', userSchema);
 
-User.comparePassword = function(candidatePassword, savedPassword, cb) {
-  // console.log()
+userSchema.methods.comparePassword = function(candidatePassword, savedPassword, cb) {
   bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
-    if (err) { return cb(err); }
+    if (err) {
+      return cb(err);
+    }
     cb(null, isMatch);
   });
 };
@@ -21,14 +21,13 @@ User.comparePassword = function(candidatePassword, savedPassword, cb) {
 userSchema.pre('save', function(next) {
   var cipher = Promise.promisify(bcrypt.hash);
   return cipher(this.password, null, null).bind(this)
-    .then(function(hash) {
+  .then(function(hash) {
       this.password = hash;
       next();
     });
 });
 
-
-
+var User = mongoose.model('User', userSchema);
 
 
 // var User = db.Model.extend({
